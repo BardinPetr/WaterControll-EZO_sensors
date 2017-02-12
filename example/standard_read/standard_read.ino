4c72ca3bd4cd0a112b69b0d9c901a6ffb23359ce
BardinPetr <bardin.petr@gmail.com>
2017-02-12 09:33:32 +0300

    Example added

--- example/standard_read/standard_read.ino
+++ example/standard_read/standard_read.ino
@@ -3,10 +3,20 @@ WaterControll sens;
 
 void setup() {
   Serial.begin(9600);
-  sens.Add(0, "pH", 3);
+  sens.Add(sens.PH, "pH", 33);
+  sens.Add(sens.OPR, "OPR");
+
+  sens.set_finishHandler(finish);
+}
+
+void finish(float* in){
+  Serial.println("Data from sensors recieved!");
+  for (int i = 0; i < sens.sensors_count; i++){
+    Serial.println(sens.sensors_name[i] + ": " + String(in[i]));
+  }
 }
 
 void loop() {
-  
+  sens.readAll_nw();
 }
 
